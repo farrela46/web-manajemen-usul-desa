d703444b610f5d4809fda49478efe857b7a42bd9
farrela46 <gpay46@gmail.com>
2024-05-13 15:26:20 +0700

    Add Template Pages

---
+++ fe-vue-manajemen-usulan-desa/src/views/TEMPLATE.vue
@@ -0,0 +1,96 @@
+<script>
+// import { useRoute } from "vue-router";
+import axios from "axios";
+import BASE_URL from '@/api/config-api';
+import ArgonButton from "@/components/ArgonButton.vue";
+import Breadcrumbs from '@/components/Vuetify/Breadcrumbs.vue';
+
+export default {
+  components: {
+    ArgonButton,
+    Breadcrumbs
+  },
+  data() {
+    return {
+      products: [],
+      overlay: false,
+      breadcrumbsItems: [
+        {
+          title: 'Home',
+          disabled: false,
+          href: '/dashboard',
+        },
+        {
+          title: '',
+          disabled: true,
+          href: '/',
+        }
+      ],
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+    goUsulan() {
+      this.$router.push('/usulan');
+    },
+    formatPrice(price) {
+      const numericPrice = parseFloat(price);
+      return numericPrice.toLocaleString('id-ID');
+    },
+    async retrieveBuku() {
+      try {
+        this.overlay = true;
+        const response = await axios.get(`${BASE_URL}/buku/get`, {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem('access_token')
+          }
+        });
+        this.products = response.data;
+
+        if (response.data.length > 0) {
+          this.fotoUrl = response.data[0].foto;
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.overlay = false
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="py-4 container-fluid">
+    <div class="row mt-3">
+      <v-overlay :model-value="overlay" class="d-flex align-items-center justify-content-center">
+        <v-progress-circular color="primary" size="96" indeterminate></v-progress-circular>
+      </v-overlay>
+      <div class="container">
+        <Breadcrumbs class="d-flex align-items-center" :items="breadcrumbsItems" />
+        <div class="row">
+          <div class="col-12">
+            <div class="card px-4" style="border-radius: 10px; 
+            /* background-color: #E9F5E9; */
+            ">
+              <div class="row mt-2 mb-2" style="border-bottom: 1px solid black;">
+                <div class="d-flex align-items-center mt-2" style="border-bottom: 1px solid black;">
+                  Menu
+                </div>
+                <div class="row">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+
+
+</style>

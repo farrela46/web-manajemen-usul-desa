71db96b4b060ab4ec0a4424be3ce396cd7e13354
yasfaa <yasfanaruto@gmail.com>
2024-06-08 10:04:53 +0700

    feat add seeders

--- BE-usulan-desa/database/seeders/DatabaseSeeder.php
+++ BE-usulan-desa/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
+use Database\Seeders\CommentSeeder;
+use Database\Seeders\ProgramSeeder;
+use Database\Seeders\SuggestionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +18,15 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+        ]);
+        $this->call([
+            SuggestionSeeder::class,
+        ]);
+        $this->call([
+            CommentSeeder::class,
+        ]);
+        $this->call([
+            ProgramSeeder::class,
         ]);
     }
 }

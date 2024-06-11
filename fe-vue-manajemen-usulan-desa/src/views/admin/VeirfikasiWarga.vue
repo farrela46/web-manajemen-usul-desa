6bb9cb923944a9b65c7ae909462eaedebd2a8208
farrela46 <gpay46@gmail.com>
2024-06-11 11:53:42 +0700

    Fix: fix typo on verifikasi warga

--- fe-vue-manajemen-usulan-desa/src/views/admin/VeirfikasiWarga.vue
+++ fe-vue-manajemen-usulan-desa/src/views/admin/VeirfikasiWarga.vue
@@ -302,7 +302,7 @@ export default {
         <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title text-black" id="deleteConfirmationModalLabel">Konfirmasi Upvote</h5>
+              <h5 class="modal-title text-black" id="deleteConfirmationModalLabel">Konfirmasi</h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
